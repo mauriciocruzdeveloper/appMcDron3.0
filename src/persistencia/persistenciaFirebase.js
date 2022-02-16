b5f465a616884a1f4c6eade5e8a0d401e28f0846
Mauricio Cruz <mauricio11111@gmail.com>
2022-02-15 22:11:44 -0300

    A

--- src/persistencia/persistenciaFirebase.js
+++ src/persistencia/persistenciaFirebase.js
@@ -25,8 +25,8 @@ import {
     CACHE_SIZE_UNLIMITED // constante para caché ilimitada
 } from "firebase/firestore";
 
-import { config as firebaseConfig }  from '../configProd'; // Para producción
-// import { config as firebaseConfig }  from '../configDev'; // Para desarrollo
+// import { config as firebaseConfig }  from '../configProd'; // Para producción
+import { config as firebaseConfig }  from '../configDev'; // Para desarrollo
 
 import { provincias } from '../datos/provincias.json';
 
