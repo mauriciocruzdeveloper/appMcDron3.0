e49a7beadc89699f9211ca7563309efdab184806
Mauricio Cruz <mauricio11111@gmail.com>
2022-02-18 23:04:56 -0300

    PRIMER CONTAINER-PRESENTATIONAL EN USUARIO

--- src/components/Usuario.presentational.jsx
+++ src/components/Usuario.presentational.jsx
@@ -1,149 +1,20 @@
-import { useEffect, useCallback, useState } from "react";
-import { connect } from "react-redux";
-import { 
-    changeInputUsu,
-    // Cliente y Usuario es lo mismo, pero Usuario se usa para referirse
-    // al usuario logueado, y Cliente para el usuario en un ABMC
-    getCliente,
-    guardarUsuario,
-    eliminarUsuario,
-    abreModal,
-    confirm,
-    getProvinciasSelect,
-    getLocalidadesPorProvincia,
-    setProvinciaCliente,
-    setLocalidadCliente,
-    clearForm,
-    // setCliente
-  } from "../redux/root-actions";
-
-import { useParams } from "react-router-dom";
-
-import history from "../history";
-
+import { jsonEval } from '@firebase/util';
 import Select from 'react-select';
 
-const Reparacion = ({ 
-    // changeInputUsu, 
-    getCliente,
-    // Cliente es el usuario que se está mostrando, usuario es el logueado
-    // cliente,
-    guardarUsuario,
-    eliminarUsuario,
-    abreModal,
-    confirm,
+const UsuarioPresentational = ({ 
+    cliente,
     provinciasSelect,
     localidadesSelect,
-    getProvinciasSelect,
-    getLocalidadesPorProvincia,
-    // setProvinciaCliente, // PARA REDUX
-    // setLocalidadCliente, // PARA REDUX
-    // clearForm, // PARA REDUX
-    coleccionUsuarios,
-    // setCliente
+    handleGuardarUsuario,
+    handleEliminarUsuario,
+    changeInputUsu,
+    handleOnChangeProvincias,
+    handleOnChangeLocalidades
 }) => {
 
-    console.log("USUARIO");
-
-    const { id } = useParams();
-
-    // Esto inicializa el form al montar y limpia al desmontar ///////
-
-    // Inicializa los datos del formulario.
-    const inicializaFormulario = useCallback(async () => {
-        // Busca los datos en caso que no estén en el store.
-        !provincias?.length ? await getProvinciasSelect() : null;
-        coleccionUsuarios.length 
-        ? setCliente(coleccionUsuarios.find(usuario => usuario.id == id))
-        : await getCliente(id)
-                .catch(error => {
-                    abreModal("Error buscando Cliente ", `Código - ${error.code}`, "danger" );
-                    history.goBack();
-                });
-    }, [id]);
-
-    useEffect(() => {
-        inicializaFormulario();
-        // return () => clearForm(); // PARA REDUX
-    }, [inicializaFormulario]);
-
-    ///////////////////////////////////////////////////////////////////
-
-    const [ cliente, setCliente ] = useState();
-
-    const changeInputUsu = target => setCliente({ 
-        ...cliente, 
-        data: {
-            ...cliente.data,
-            [target.id]: target.value
-        } 
-    });
     
-    ///////////////////////////////////////////////////////////////////
 
-    const handleGuardarUsuario = () => {
-        confirm(
-            "Guardar Usuario?",
-            "Atención",
-            "warning",
-            () => {
-                guardarUsuario(cliente)
-                .then(reparacion => abreModal("Guardado con éxito", "Usuario: " + cliente.id, "success" ))
-                //.catch(error => abreModal("Error al guardar ", "Código - " + error.code, "danger" ));
-            }
-        );
-    }
-
-    const handleEliminarUsuario = () => {
-        confirm(
-            "Eliminar Reparación?",
-            "Atención",
-            "danger",
-            () => {
-                console.log("llega al callBakc de confirm") ;
-                eliminarUsuario(cliente.id)
-                .then(id => {
-                    abreModal("Usuario eliminado con éxito", "Usuario: " + id, "success" );
-                    history.goBack();
-                })
-                .catch(error => abreModal("Error al guardar ", "Código - " + error.code, "danger" ))
-            }
-        );
-    }
-
-    // PARA REDUX
-    // const handleOnChangeProvincias = async (e) => {
-    //     await getLocalidadesPorProvincia(e.value);
-    //     await setProvinciaCliente(e.value);
-    // }
-
-    // PARA USESTATE
-    const handleOnChangeProvincias = async (e) => {
-        await getLocalidadesPorProvincia(e.value);
-        setCliente({ 
-            ...cliente, 
-            data: {
-                ...cliente.data,
-                ProvinciaUsu: e.value
-            } 
-        });
-    }
-
-    // PARA REDUX
-    // const handleOnChangeLocalidades = async (e) => {
-    //     await setLocalidadCliente(e.value);
-    // }
-
-    // PARA USESTATE
-    const handleOnChangeLocalidades = (e) => {
-        setCliente({ 
-            ...cliente, 
-            data: {
-                ...cliente.data,
-                LocalidadUsu: e.value
-            } 
-        });
-    }
+    console.log("USUARIO presentational");
 
     return(
         <div
@@ -214,7 +85,7 @@ const Reparacion = ({
                     <div>
                         <label className="form-label">Provincia</label>
                         <Select 
-                            options={provincias}
+                            options={provinciasSelect}
                             onChange={e => handleOnChangeProvincias(e)}
                             id="ProvinciaUsu"
                             // Hay que usar "value" y no "defaultValue". Por alguna razón que desconozco
@@ -225,7 +96,7 @@ const Reparacion = ({
                     <div>
                         <label className="form-label">Ciudad</label>
                         <Select 
-                            options={localidades}
+                            options={localidadesSelect}
                             onChange={e => handleOnChangeLocalidades(e)}
                             id="CiudadUsu"
                             // Ver si puedo usar useRef para habilitar y desabilitar
@@ -256,27 +127,4 @@ const Reparacion = ({
     )
 }
 
-const mapStateToProps = (state) => ({
-    cliente: state.app?.cliente,
-    provinciasSelect: state.app?.provinciasSelect,
-    localidadesSelect: state.app?.localidadesSelect,
-    coleccionUsuarios: state.app?.coleccionUsuarios
-  });
-
-
-export default connect(
-    mapStateToProps, 
-    {
-        changeInputUsu,
-        getCliente,
-        guardarUsuario,
-        eliminarUsuario, 
-        abreModal,
-        confirm,
-        getProvinciasSelect,
-        getLocalidadesPorProvincia,
-        setProvinciaCliente,
-        setLocalidadCliente,
-        clearForm,
-        setCliente
-    })(Reparacion);
+export default UsuarioPresentational;
