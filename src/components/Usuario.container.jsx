7ac6ff29e25363adc6992faa596b92bf70af34ed
Mauricio Cruz <mauricio11111@gmail.com>
2022-02-18 23:05:21 -0300

    PRIMER CONTAINER-PRESENTATIONAL EN USUARIO

--- src/components/Usuario.container.jsx
+++ src/components/Usuario.container.jsx
@@ -1,11 +1,15 @@
+//React
 import { useEffect, useCallback, useState } from "react";
+//
 import { connect } from "react-redux";
+import { useParams } from "react-router-dom";
+import history from "../history";
+//Actions
 import { 
     changeInputUsu,
     // Cliente y Usuario es lo mismo, pero Usuario se usa para referirse
     // al usuario logueado, y Cliente para el usuario en un ABMC
     getCliente,
-    setEstado,
     guardarUsuario,
     eliminarUsuario,
     abreModal,
@@ -15,36 +19,34 @@ import {
     setProvinciaCliente,
     setLocalidadCliente,
     clearForm,
-    setCliente
+    // setCliente
   } from "../redux/root-actions";
-
-import { useParams } from "react-router-dom";
-
-import history from "../history";
-
+//Components
 import Select from 'react-select';
+import UsuarioPresentational from './Usuario.presentational'
 
-const Reparacion = ({ 
+
+const Usuario = ({ 
     // changeInputUsu, 
     getCliente,
     // Cliente es el usuario que se está mostrando, usuario es el logueado
-    // cliente,
+    // cliente, // PARA REDUX
     guardarUsuario,
     eliminarUsuario,
     abreModal,
     confirm,
-    provincias,
-    localidades,
+    provinciasSelect,
+    localidadesSelect,
     getProvinciasSelect,
     getLocalidadesPorProvincia,
-    setProvinciaCliente,
-    setLocalidadCliente,
-    clearForm,
+    // setProvinciaCliente, // PARA REDUX
+    // setLocalidadCliente, // PARA REDUX
+    // clearForm, // PARA REDUX
     coleccionUsuarios,
     // setCliente
 }) => {
 
-    console.log("USUARIO");
+    console.log("USUARIO container");
 
     const { id } = useParams();
 
@@ -53,7 +55,7 @@ const Reparacion = ({
     // Inicializa los datos del formulario.
     const inicializaFormulario = useCallback(async () => {
         // Busca los datos en caso que no estén en el store.
-        !provincias?.length ? await getProvinciasSelect() : null;
+        !provinciasSelect?.length ? await getProvinciasSelect() : null;
         coleccionUsuarios.length 
         ? setCliente(coleccionUsuarios.find(usuario => usuario.id == id))
         : await getCliente(id)
@@ -65,7 +67,7 @@ const Reparacion = ({
 
     useEffect(() => {
         inicializaFormulario();
-        return () => clearForm();
+        // return () => clearForm(); // PARA REDUX
     }, [inicializaFormulario]);
 
     ///////////////////////////////////////////////////////////////////
@@ -112,130 +114,58 @@ const Reparacion = ({
         );
     }
 
+    // PARA REDUX
+    // const handleOnChangeProvincias = async (e) => {
+    //     await getLocalidadesPorProvincia(e.value);
+    //     await setProvinciaCliente(e.value);
+    // }
+
+    // PARA USESTATE
     const handleOnChangeProvincias = async (e) => {
         await getLocalidadesPorProvincia(e.value);
-        await setProvinciaCliente(e.value);
+        setCliente({ 
+            ...cliente, 
+            data: {
+                ...cliente.data,
+                ProvinciaUsu: e.value
+            } 
+        });
     }
 
-    const handleOnChangeLocalidades = async (e) => {
-        await setLocalidadCliente(e.value);
+    // PARA REDUX
+    // const handleOnChangeLocalidades = async (e) => {
+    //     await setLocalidadCliente(e.value);
+    // }
+
+    // PARA USESTATE
+    const handleOnChangeLocalidades = (e) => {
+        setCliente({ 
+            ...cliente, 
+            data: {
+                ...cliente.data,
+                CiudadUsu: e.value
+            } 
+        });
     }
 
     return(
-        <div
-            className="p-4"
-            style={{
-                backgroundColor: "#EEEEEE"
-            }}
-        >
-            <div 
-                className="card mb-3"
-                style={{
-                    backgroundColor: "#CCCCCC",
-
-                }}
-            >
-                <div className="card-body">
-                    <h3 className="card-title">
-                        USUARIO
-                    </h3>
-                    <div>Nombre: {cliente?.data?.NombreUsu} {cliente?.data?.ApellidoUsu}</div>
-                    <div>Email: {cliente?.data?.EmailUsu}</div>
-                </div>
-            </div>
-
-            <div className="card mb-3">
-                <div className="card-body">
-                <h5 className="card-title bluemcdron">DATOS DEL USUARIO</h5>
-                <div>
-                        <label className="form-label">E-mail</label>
-                        <input 
-                            onChange={e => changeInputUsu(e.target)} 
-                            type="text" 
-                            className="form-control" 
-                            id="EmailUsu" 
-                            value={cliente?.data?.EmailUsu || ""}
-                        />
-                    </div>
-                    <div>
-                        <label className="form-label">Nombre</label>
-                        <input 
-                            onChange={e => changeInputUsu(e.target)} 
-                            type="text" 
-                            className="form-control" 
-                            id="NombreUsu" 
-                            value={cliente?.data?.NombreUsu || ""}
-                        />
-                    </div>
-                    <div>
-                        <label className="form-label">Apellido</label>
-                        <input 
-                            onChange={e => changeInputUsu(e.target)} 
-                            type="text" 
-                            className="form-control" 
-                            id="ApellidoUsu" 
-                            value={cliente?.data?.ApellidoUsu || ""}
-                        />
-                    </div>
-                    <div>
-                        <label className="form-label">Teléfono</label>
-                        <input 
-                            onChange={e => changeInputUsu(e.target)} 
-                            type="tel" 
-                            className="form-control" 
-                            id="TelefonoUsu"
-                            value={cliente?.data?.TelefonoUsu || ""}
-                        />
-                    </div>
-                    <div>
-                        <label className="form-label">Provincia</label>
-                        <Select 
-                            options={provincias}
-                            onChange={e => handleOnChangeProvincias(e)}
-                            id="ProvinciaUsu"
-                            // Hay que usar "value" y no "defaultValue". Por alguna razón que desconozco
-                            // defaultValue me trae el valor del estado anterior...
-                            value={({value: cliente?.data?.ProvinciaUsu, label: cliente?.data?.ProvinciaUsu})}
-                        />
-                    </div>
-                    <div>
-                        <label className="form-label">Ciudad</label>
-                        <Select 
-                            options={localidades}
-                            onChange={e => handleOnChangeLocalidades(e)}
-                            id="CiudadUsu"
-                            // Ver si puedo usar useRef para habilitar y desabilitar
-                            // el campo luego de elegir la provincia
-                            // ref="CiudadUsu"
-                            value={{value: cliente?.data?.CiudadUsu, label: cliente?.data?.CiudadUsu}}
-                        />
-                    </div>
-                </div>
-            </div>
-
-
-           <div className="text-center">
-                <button 
-                    onClick={ handleGuardarUsuario }
-                    className="w-100 mb-3 btn bg-bluemcdron text-white"
-                >
-                    Guardar
-                </button>
-                <button 
-                    onClick={ handleEliminarUsuario }
-                    className="w-100 btn bg-danger text-white"
-                >
-                    Eliminar
-                </button>
-            </div>
-        </div>
+        <UsuarioPresentational 
+            cliente={cliente}
+            provinciasSelect={provinciasSelect}
+            localidadesSelect={localidadesSelect}
+            handleGuardarUsuario={handleGuardarUsuario}
+            handleEliminarUsuario={handleEliminarUsuario}
+            changeInputUsu={changeInputUsu}
+            handleOnChangeProvincias={handleOnChangeProvincias}
+            handleOnChangeLocalidades={handleOnChangeLocalidades}
+        />
     )
 }
 
 const mapStateToProps = (state) => ({
     cliente: state.app?.cliente,
-    provincias: state.app?.provincias,
-    localidades: state.app?.localidades,
+    provinciasSelect: state.app?.provinciasSelect,
+    localidadesSelect: state.app?.localidadesSelect,
     coleccionUsuarios: state.app?.coleccionUsuarios
   });
 
@@ -245,7 +175,6 @@ export default connect(
     {
         changeInputUsu,
         getCliente,
-        setEstado, 
         guardarUsuario,
         eliminarUsuario, 
         abreModal,
@@ -255,5 +184,5 @@ export default connect(
         setProvinciaCliente,
         setLocalidadCliente,
         clearForm,
-        setCliente
-    })(Reparacion);
+        // setCliente // PARA REDUX
+    })(Usuario);
