f146b35eb79753ced4c35f7946f7c44ce276f31b
Mauricio Cruz <mauricio11111@gmail.com>
2024-12-27 18:06:01 -0300

    feat: Implementar función para enviar email vacío en la lógica de reparaciones

--- src/utils/sendEmails.ts
+++ src/utils/sendEmails.ts
@@ -5,11 +5,24 @@ import { enviarEmail } from "./utils";
 
 export const enviarRecibo = (reparacion: ReparacionType): void => {
     const datosEmail = {
+        from: EMAIL_REPARACIONES,
         to: reparacion.data.EmailUsu,
         cc: EMAIL_REPARACIONES,
         bcc: [],
         subject: 'Recibo de equipo ' + reparacion.data.DroneRep,
         body: bodyRecibo(reparacion),
+    };
+    enviarEmail(datosEmail);
+}
+
+export const enviarEmailVacio = (reparacion: ReparacionType): void => {
+    const datosEmail = {
+        from: EMAIL_REPARACIONES,
+        to: reparacion.data.UsuarioRep,
+        cc: EMAIL_REPARACIONES,
+        bcc: [],
+        subject: '',
+        body: ''
     };
     enviarEmail(datosEmail);
 }
