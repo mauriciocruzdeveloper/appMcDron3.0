304dd68b4b018dd8adb78b65db867e69cb7064f1
Mauricio Cruz <mauricio11111@gmail.com>
2024-12-27 19:23:59 -0300

    feat: Agregar información importante sobre diagnóstico y presupuesto en el email de recibido

--- src/emails/recibido.ts
+++ src/emails/recibido.ts
@@ -7,9 +7,18 @@ Fecha de ingreso: ${new Date(reparacion.data.FeRecRep).toLocaleDateString()}
 Observaciones: ${reparacion.data.DescripcionUsuRep}
 Cliente: ${reparacion.data.NombreUsu} ${reparacion.data.ApellidoUsu}
 Teléfono: ${reparacion.data.TelefonoUsu}
-        
+
+IMPORTANTE:
+- El diagnóstico y presupesto demorará hasta 48 hs hábiles a partir de la fecha de ingreso del equipo.
+- En caso de aceptar el presupuesto, el tiempo de reparación es aproximadamente de 5 a 7 días hábiles.
+- En caso de no aceptar el presupuesto, el costo del diagnóstico es de U$S 25.
+- La reparación tiene 90 días de garantía.
+
+
 Mauricio Cruz Drones
 www.mauriciocruzdrones.com
 Teléfono: +54 9 341 7439091
-Email: mauriciocruzdrones@gmail.com`;
+Email reparaciones: reparaciones@mauriciocruzdrones.com
+Email información general: info@mauriciocruzdrones.com
+`;
 }
