91cb1d9cf54c8912e71e1bf272209bd0e3b53e5f
Mauricio Cruz <mauricio11111@gmail.com>
2024-12-27 17:43:36 -0300

    feat: Refactor lógica de envío de emails para utilizar constantes de dirección

--- src/utils/sendEmails.ts
+++ src/utils/sendEmails.ts
@@ -1,11 +1,12 @@
 import { bodyRecibo } from "../emails/recibido";
+import { EMAIL_REPARACIONES } from "../types/constantes";
 import { ReparacionType } from "../types/reparacion";
 import { enviarEmail } from "./utils";
 
 export const enviarRecibo = (reparacion: ReparacionType): void => {
     const datosEmail = {
         to: reparacion.data.EmailUsu,
-        cc: 'reparaciones@mauriciocruzdrones.com',
+        cc: EMAIL_REPARACIONES,
         bcc: [],
         subject: 'Recibo de equipo ' + reparacion.data.DroneRep,
         body: bodyRecibo(reparacion),
