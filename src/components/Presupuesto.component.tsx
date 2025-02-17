0b56dc80abf7d96af486830922385c828c064fbf
Mauricio Cruz <mauricio11111@gmail.com>
2025-02-17 17:05:23 -0300

    refactor: Reemplazar acciones obsoletas de Redux y mover funciones a utils

--- src/components/Presupuesto.component.tsx
+++ src/components/Presupuesto.component.tsx
@@ -8,7 +8,7 @@ import { useAppDispatch } from "../redux-tool-kit/hooks/useAppDispatch";
 import { useModal } from "./Modal/useModal";
 import { guardarPresupuestoAsync } from "../redux-tool-kit/reparacion/reparacion.actions";
 import { getClienteAsync } from "../redux-tool-kit/usuario/usuario.actions";
-import { getLocalidadesPorProvincia, getProvinciasSelect } from "../redux-DEPRECATED/App/App.actions";
+import { getLocalidadesPorProvincia, getProvinciasSelect } from "../utils/utils";
 
 // import { provincias } from '../datos/provincias.json'; 
 
