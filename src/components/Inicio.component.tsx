c276a4dfb1343666317a551d862162edff898117
Mauricio Cruz <mauricio11111@gmail.com>
2025-08-17 21:45:47 -0300

    feat: Refactor componente de navegación y eliminar el componente Inicio

--- src/components/Inicio.component.tsx
+++ src/components/Inicio.component.tsx
@@ -39,92 +39,9 @@ const Inicio = (props: InicioProps): React.ReactElement => {
     <div className='p-4'>
       <img className='mb-4' src='./img/logo.png' alt='' width='100%' max-width='100px' />
 
-      {/* Grupo 1: Recepción - primer proceso del negocio */}
-      <div className="mb-4 text-center text-muted">
-        <small>Procesos de Negocio</small>
-        <hr className="mt-1" />
-      </div>
-
       {/* Ahora usamos la función renderEstadoButton para generar los botones de estado */}
       {renderEstadoButton('Recibido', 'RECEPCIÓN')}
       {renderEstadoButton('Transito', 'DRONE EN TRÁNSITO')}
-
-      {/* Grupo 2: Gestión y administración */}
-      <div className="mb-4 text-center text-muted">
-        <small>Gestión y Administración</small>
-        <hr className="mt-1" />
-      </div>
-      <button
-        className='mb-3 btn w-100 bg-bluemcdron'
-        style={{ height: '100px' }}
-        onClick={() => history.push(`${match.path}/reparaciones`)}
-      >
-        <div className='text-white text-center'>REPARACIONES</div>
-      </button>
-
-      {admin ?
-        <button
-          className='mb-3 btn w-100 bg-bluemcdron'
-          style={{ height: '100px' }}
-          onClick={() => history.push(`${match.path}/usuarios`)}
-        >
-          <div className='text-white text-center'>USUARIOS</div>
-        </button>
-        : null}
-
-      {admin ?
-        <button
-          className='mb-3 btn w-100 bg-bluemcdron'
-          style={{ height: '100px' }}
-          onClick={() => history.push(`${match.path}/repuestos`)}
-        >
-          <div className='text-white text-center'>REPUESTOS</div>
-        </button>
-        : null}
-
-      {admin ?
-        <button
-          className='mb-3 btn w-100 bg-bluemcdron'
-          style={{ height: '100px' }}
-          onClick={() => history.push(`${match.path}/modelos-drone`)}
-        >
-          <div className='text-white text-center'>MODELOS DE DRONES</div>
-        </button>
-        : null}
-
-      {admin ?
-        <button
-          className='mb-3 btn w-100 bg-bluemcdron'
-          style={{ height: '100px' }}
-          onClick={() => history.push(`${match.path}/drones`)}
-        >
-          <div className='text-white text-center'>DRONES</div>
-        </button>
-        : null}
-
-      {admin ?
-        <button
-          className='mb-3 btn w-100 bg-bluemcdron'
-          style={{ height: '100px' }}
-          onClick={() => history.push(`${match.path}/intervenciones`)}
-        >
-          <div className='text-white text-center'>INTERVENCIONES</div>
-        </button>
-        : null}
-
-      {/* Grupo 3: Comunicación */}
-      <div className="mb-4 text-center text-muted">
-        <small>Comunicación</small>
-        <hr className="mt-1" />
-      </div>
-
-      <button
-        className='mb-3 btn w-100 bg-bluemcdron'
-        style={{ height: '100px' }}
-        onClick={() => history.push(`${match.path}/mensajes`)}
-      >
-        <div className='text-white text-center'>MENSAJES</div>
-      </button>
     </div>
   )
 };
